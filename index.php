19f4d6342e510942a4342db10ac4484e9647eefb
subhajitroy <subhajit.r@gmail.com>
2023-03-30 12:23:48 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+  <h1><b>P</b>rogramming Languages, Automated <b>R</>easoning, <b>A</b>rtificial <b>I</b>ntelligence and <b>S</b>oftware <b>E</b>ngineering</h1>
+
+<?php
+echo "Welcome to PRAISE!";
+?>
+
+</body>
+</html> 
